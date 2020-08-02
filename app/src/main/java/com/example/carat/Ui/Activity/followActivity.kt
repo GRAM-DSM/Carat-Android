8dc556654029d41395a46e8285e45feb9bf916d5
sirasatarato <sirasatarato@gmail.com>
2020-08-02 14:28:03 +0900

    Create followActivity

---
+++ app/src/main/java/com/example/carat/Ui/Activity/followActivity.kt
@@ -0,0 +1,11 @@
+package com.example.carat.Ui.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class followActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follow)
+    }
+}
