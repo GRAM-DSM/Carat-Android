7fc6a84a18d3ca4c016d8a03ad7a9da6edbcacc4
sirasatarato <sirasatarato@gmail.com>
2020-08-02 15:02:38 +0900

    Create ChangeProfile Activity

--- app/src/main/java/com/example/carat/Ui/Activity/ChangeProfile.kt
+++ app/src/main/java/com/example/carat/Ui/Activity/ChangeProfile.kt
@@ -2,10 +2,11 @@ package com.example.carat.Ui.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.carat.R
 
-class followActivity : AppCompatActivity() {
+class ChangeProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_follow)
+        setContentView(R.layout.activity_change_profile)
     }
 }
