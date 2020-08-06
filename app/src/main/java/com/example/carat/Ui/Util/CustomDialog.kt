f50d54f07bfd72cdd4ad768deebc4e8c3f49f2e1
sirasatarato <sirasatarato@gmail.com>
2020-08-06 14:29:41 +0900

    Create Logout Dialog

---
+++ app/src/main/java/com/example/carat/Ui/Util/CustomDialog.kt
@@ -0,0 +1,39 @@
+package com.example.carat.Ui.Util
+
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import com.example.carat.R
+import kotlinx.android.synthetic.main.layout_dialog.*
+import java.util.*
+
+class CustomDialog(
+    context: Context?,
+    private val mLeftClickListener: View.OnClickListener?,
+    private val mRightClickListener: View.OnClickListener?
+) : Dialog(context!!, android.R.style.Theme_Translucent_NoTitleBar) {
+
+    override fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+
+        blurringTheBackground()
+        settingTheClickEvents()
+    }
+
+    private fun blurringTheBackground() {
+        val lpWindow = WindowManager.LayoutParams()
+        lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND
+        lpWindow.dimAmount = 0.8f
+        Objects.requireNonNull(window)?.attributes = lpWindow
+        setContentView(R.layout.layout_dialog)
+    }
+
+    private fun settingTheClickEvents() {
+        if (mLeftClickListener != null && mRightClickListener != null) {
+            mLeftButton.setOnClickListener(mLeftClickListener)
+            mRightButton.setOnClickListener(mRightClickListener)
+        }
+    }
+}
