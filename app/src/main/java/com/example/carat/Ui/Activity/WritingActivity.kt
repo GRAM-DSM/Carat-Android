3f935b59d422bf377b9053f87c610b5f0b10bc5f
sirasatarato <sirasatarato@gmail.com>
2020-08-07 15:57:12 +0900

    Make work to save  the data

--- app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
+++ app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
@@ -10,16 +10,21 @@ import kotlinx.android.synthetic.main.activity_writing.*
 import kotlinx.android.synthetic.main.widget_appbar.view.*
 
 class WritingActivity : AppCompatActivity() {
+    private val appbar: Toolbar by lazy {
+        writing_appbar_include.widget_toolbar
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing)
 
         settingActionBar()
         setProfileImageForm()
+        setClickEvents()
     }
 
     private fun settingActionBar() {
-        writing_appbar_include.widget_toolbar.apply {
+        appbar.apply {
             setSupportActionBar(this)
             appbar_backKey_imageView.setImageResource(R.drawable.icon_close_button)
             appbar_title_textView.visibility = View.GONE
@@ -32,5 +37,21 @@ class WritingActivity : AppCompatActivity() {
             .load(R.drawable.image_default_profile)
             .circleCrop()
             .into(writing_profile_imageView)
+    }
+
+    private fun setClickEvents() {
+        appbar.apply {
+            appbar_backKey_imageView.setOnClickListener {
+                finish()
+            }
+            appbar_save_textView.setOnClickListener {
+                saveContent()
+                finish()
+            }
+        }
+    }
+
+    private fun saveContent() {
+        TODO()
     }
 }
