1804c908b7f48dd660ea075dd528d33f54f52a11
sirasatarato <sirasatarato@gmail.com>
2020-08-07 14:59:01 +0900

    Make writing appbar setting work

--- app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
+++ app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
@@ -2,11 +2,31 @@ package com.example.carat.Ui.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import com.example.carat.R
+import kotlinx.android.synthetic.main.activity_writing.*
+import kotlinx.android.synthetic.main.widget_appbar.view.*
 
 class WritingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing)
+
+        settingActionBar()
+        setProfileImageForm()
+    }
+
+    fun settingActionBar() {
+        writing_appbar_include.widget_toolbar.apply {
+            setSupportActionBar(this)
+            appbar_backKey_imageView.setImageResource(R.drawable.icon_close_button)
+            appbar_title_textView.visibility = View.GONE
+            appbar_save_textView.text = "캐링하기"
+        }
+    }
+
+    fun setProfileImageForm() {
+        
     }
 }
