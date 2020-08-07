780cdc9245c2a791afef9c3efbf90af33d5769e7
sirasatarato <sirasatarato@gmail.com>
2020-08-07 09:07:58 +0900

    Create Writing Activity

---
+++ app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.carat.Ui.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.carat.R
+
+class WritingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writing)
+    }
+}
