fc3e25f8861da789c5e95eb12f1794ea68bcd277
sirasatarato <sirasatarato@gmail.com>
2020-08-11 13:28:12 +0900

    Create Retrofit Object

---
+++ app/src/main/java/com/example/carat/Model/TokenData.kt
@@ -0,0 +1,6 @@
+package com.example.carat.Model
+
+object TokenData {
+    var access_token: String = ""
+    var refresh_token: String = ""
+}
---
+++ app/src/main/java/com/example/carat/Network/CaratService.kt
@@ -0,0 +1,3 @@
+package com.example.carat.Network
+
+interface CaratService {}
