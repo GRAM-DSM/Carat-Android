049edb5b735f04035d9510029a745ba2ca3040cb
sirasatarato <sirasatarato@gmail.com>
2020-08-11 18:21:53 +0900

    Delete Logout api

--- app/src/main/java/com/example/carat/Network/CaratService.kt
+++ app/src/main/java/com/example/carat/Network/CaratService.kt
@@ -1,9 +1,7 @@
 package com.example.carat.Network
 
-import com.example.carat.Model.ServerMessage
 import com.example.carat.Model.TokenData
 import okhttp3.ResponseBody
-import retrofit2.http.DELETE
 import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -11,9 +9,6 @@ import retrofit2.http.POST
 interface CaratService {
     @POST("/user/auth")
     suspend fun login(@Field("password") password: String, @Field("email") email: String) : TokenData
-
-    @DELETE("/user/auth")
-    suspend fun logout() : ServerMessage
 
     @GET("/user/auth")
     suspend fun reissueToken() : ResponseBody
