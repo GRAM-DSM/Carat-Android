5821c06e096bf4e39db8a8d4cd8b046e7edb4faf
Anminhee <jwmh15@gmail.com>
2020-08-11 09:29:26 +0900

    Create Timeline

---
+++ app/src/main/java/com/example/carat/Presenter/SignInUp/CreateUserPresenter.kt
@@ -0,0 +1,24 @@
+package com.example.carat.Presenter.SignInUp
+
+import com.example.carat.Presenter.SignInUp.CreateUserContract
+
+class CreateUserPresenter:
+    CreateUserContract.Presenter {
+
+    override fun saveData() {
+
+    }
+
+    override fun makeToast(msg: String) {
+        makeToast(msg)
+    }
+
+    override fun sendDataToServer() {
+
+    }
+
+    override fun sendDataToLogin() {
+
+    }
+
+}
