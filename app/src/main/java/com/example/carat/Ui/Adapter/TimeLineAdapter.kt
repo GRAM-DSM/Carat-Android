b45b89b5a61ba789b574c8a2ae5e12741d4c741e
minhee <jwmh15@gmail.com>
2020-08-12 18:47:47 +0900

    Add TimeLineFragment

--- app/src/main/java/com/example/carat/Ui/Adapter/TimeLineAdapter.kt
+++ app/src/main/java/com/example/carat/Ui/Adapter/TimeLineAdapter.kt
@@ -3,6 +3,7 @@ package com.example.carat.Ui.Adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carat.Model.Post
 import com.example.carat.R
@@ -20,14 +21,21 @@ class TimeLineAdapter(val postList: ArrayList<Post>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val Post: Post = postList[position]
+        val post: Post = postList[position]
 
+        holder.adapter_email_textView.text= post.email
+        holder.adapter_name_textView.text= post.name
+        holder.adapter_time_textView.text = post.time
+        holder.adapter_content_textView.text = post.content
+        holder.adapter_link_textView.text = post.link
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-
+        val adapter_name_textView = itemView.findViewById<TextView>(R.id.itemCarat_name_textView)
+        val adapter_email_textView = itemView.findViewById<TextView>(R.id.itemCarat_email_textView)
+        val adapter_time_textView = itemView.findViewById<TextView>(R.id.itemCarat_time_textView)
+        val adapter_content_textView = itemView.findViewById<TextView>(R.id.itemCarat_content_textView)
+        val adapter_link_textView = itemView.findViewById<TextView>(R.id.itemCarat_link_textView)
     }
 
-    
 }
