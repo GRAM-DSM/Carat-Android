ff2de0da7bb2b156f48a790b08253d9c62b8ba5f
sirasatarato <sirasatarato@gmail.com>
2020-08-16 20:38:37 +0900

    Create Repository

---
+++ app/src/main/java/com/example/carat/Repository/Repository.kt
@@ -0,0 +1,5 @@
+package com.example.carat.Repository
+
+class Repository {
+
+}
---
+++ app/src/main/java/com/example/carat/Repository/SharedPreferencesManager.kt
@@ -0,0 +1,8 @@
+package com.example.carat.Repository
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SharedPreferencesManager private constructor(context: Context) {
+    
+}
