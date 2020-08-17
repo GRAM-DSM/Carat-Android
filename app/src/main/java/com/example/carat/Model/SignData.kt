413a9e97f94a4ecfc46553ef856e792945423e83
minhee <jwmh15@gmail.com>
2020-08-17 23:26:09 +0900

    Delete login/sign object

--- app/src/main/java/com/example/carat/Model/SignData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.carat.Model
-
-object SignData {
-    var sign_name: String? = null
-    var sign_email: String? = null
-    var sign_password: String? = null
-}
--- app/src/main/java/com/example/carat/Presenter/SignInUp/CreateUserPresenter.kt
+++ app/src/main/java/com/example/carat/Presenter/SignInUp/CreateUserPresenter.kt
@@ -1,15 +1,11 @@
 package com.example.carat.Presenter.SignInUp
 
-import com.example.carat.Presenter.SignInUp.CreateUserContract
-
 class CreateUserPresenter :
     CreateUserContract.Presenter {
 
     override fun saveData() {}
     override fun makeToast(msg: String) {}
     override fun sendDataToServer() {}
-    override fun sendDataToLogin() {
-
-    }
+    override fun sendDataToLogin() {}
 
 }
