c5575bb7cc54c762674c783367ffb6b102988fcf
sirasatarato <sirasatarato@gmail.com>
2020-08-18 09:05:15 +0900

    Add navigation action

--- app/src/main/java/com/example/carat/Ui/Activity/MainActivity.kt
+++ app/src/main/java/com/example/carat/Ui/Activity/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.carat.Ui.Activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.carat.R
+import com.example.carat.Ui.Fragment.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +18,30 @@ class MainActivity : AppCompatActivity() {
         clickWritingButton()
     }
 
-    fun clickWritingButton() {
+    private fun clickWritingButton() {
         main_writing_floatingActionButton.setOnClickListener {
             val intent = Intent(this, WritingActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragment: Fragment = when (item.itemId) {
+            // R.id.menu_home_item -> fragment
+            R.id.menu_profile_item -> ProfileFragment()
+            else -> ProfileFragment()
+        }
+        return loadFragment(fragment)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.main_show_fragmentContainerView, fragment)
+                commit()
+            }
+            return true
+        }
+        return false
     }
 }
