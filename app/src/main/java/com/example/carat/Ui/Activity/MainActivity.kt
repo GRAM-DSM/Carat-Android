d15db5322c7cc166cf411a68a0defdf643229c81
sirasatarato <sirasatarato@gmail.com>
2020-08-18 11:19:35 +0900

    Apply ViewPager

--- app/src/main/java/com/example/carat/Ui/Activity/MainActivity.kt
+++ app/src/main/java/com/example/carat/Ui/Activity/MainActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.example.carat.R
-import com.example.carat.Ui.Fragment.ProfileFragment
+import com.example.carat.Ui.Adapter.ViewPagerAdapter
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener,
+    ViewPager.OnPageChangeListener {
+    private var prevMenuItem: MenuItem? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         clickWritingButton()
+        setupViewPager()
     }
 
     private fun clickWritingButton() {
@@ -25,23 +29,28 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         }
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val fragment: Fragment = when (item.itemId) {
-            // R.id.menu_home_item -> fragment
-            R.id.menu_profile_item -> ProfileFragment()
-            else -> ProfileFragment()
-        }
-        return loadFragment(fragment)
+    private fun setupViewPager() {
+        main_page_viewpager.adapter = ViewPagerAdapter(supportFragmentManager)
     }
 
-    private fun loadFragment(fragment: Fragment?): Boolean {
-        if (fragment != null) {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.main_show_fragmentContainerView, fragment)
-                commit()
-            }
-            return true
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_home_item -> main_page_viewpager.currentItem = 0
+            R.id.menu_profile_item -> main_page_viewpager.currentItem = 1
         }
         return false
     }
+
+    override fun onPageSelected(position: Int) {
+        if (prevMenuItem != null) {
+            prevMenuItem!!.isChecked = false
+        } else {
+            main_tab_bottomNavigation.menu.getItem(0).isChecked = false
+        }
+
+        main_tab_bottomNavigation.menu.getItem(position).isChecked = true
+        prevMenuItem = main_tab_bottomNavigation.menu.getItem(position)
+    }
+    override fun onPageScrollStateChanged(state: Int) {}
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
 }
---
+++ app/src/main/java/com/example/carat/Ui/Adapter/ViewPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.carat.Ui.Adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.carat.Ui.Fragment.ProfileFragment
+
+class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                ProfileFragment()
+            }
+            else -> {
+                ProfileFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int = 2
+}
