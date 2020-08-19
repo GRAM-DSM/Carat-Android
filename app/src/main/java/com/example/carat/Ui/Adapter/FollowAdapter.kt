ec1442c3e6544f261409ba40576ec04bf4e444bf
sirasatarato <sirasatarato@gmail.com>
2020-08-19 12:15:17 +0900

    Create follow item xml

--- app/src/main/java/com/example/carat/Ui/Adapter/FollowAdapter.kt
+++ app/src/main/java/com/example/carat/Ui/Adapter/FollowAdapter.kt
@@ -7,14 +7,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.carat.Model.FollowData
 import com.example.carat.R
 
-class FollowAdapter(val list: ArrayList<FollowData>): RecyclerView.Adapter<FollowAdapter.FollowVH>() {
-    class FollowVH(view: View): RecyclerView.ViewHolder(view)
+class FollowAdapter(val list: ArrayList<FollowData>) :
+    RecyclerView.Adapter<FollowAdapter.FollowVH>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowVH
-    = FollowVH(LayoutInflater.from(parent.context).inflate(,parent,false))
+    class FollowVH(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowVH =
+        FollowVH(LayoutInflater.from(parent.context).inflate(R.layout.item_follow, parent, false))
 
     override fun getItemCount(): Int = list.size
-    override fun onBindViewHolder(holder: FollowVH, position: Int) {
 
+    override fun onBindViewHolder(holder: FollowVH, position: Int) {
+        val data: FollowData = list[position]
     }
 }
