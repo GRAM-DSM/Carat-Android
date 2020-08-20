38d46146f910cc7ad5ae711a65c2981f908ba076
sirasatarato <sirasatarato@gmail.com>
2020-08-20 11:14:42 +0900

    Add Profile section API

--- app/src/main/java/com/example/carat/Model/FollowData.kt
+++ app/src/main/java/com/example/carat/Model/FollowData.kt
@@ -1,7 +1,10 @@
 package com.example.carat.Model
 
 data class FollowData(
-    var image: String = "",
+    var num: Int = 0,
+    var profile_image: String = "",
     var name: String = "",
     var email: String = "",
+    var about_me: String = "",
+    var following: Boolean = false
 )
--- app/src/main/java/com/example/carat/Network/CaratService.kt
+++ app/src/main/java/com/example/carat/Network/CaratService.kt
@@ -2,15 +2,44 @@ package com.example.carat.Network
 
 import com.example.carat.Model.ServerMessage
 import com.example.carat.Model.TokenData
-import retrofit2.http.Field
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.POST
+import com.example.carat.Model.UserData
+import retrofit2.Call
+import retrofit2.http.*
 
 interface CaratService {
-    @POST("/user/auth")
+    @POST("user/auth")
     suspend fun login(@Field("password") password: String, @Field("email") email: String): TokenData
 
-    @GET("/user/auth")
+    @GET("user/auth")
     suspend fun reissueToken(@Header("Authorization") authorization: String): ServerMessage
+
+    @GET("profile/{email}")
+    suspend fun getProfile(
+        @Header("Authorization") authorization: String,
+        @Path("email") email: String
+    ): UserData
+
+    @GET("profile/{email}/following")
+    suspend fun getFollowingList(
+        @Header("Authorization") authorization: String,
+        @Path("email") email: String
+    )
+
+    @GET("profile/{email}/followers")
+    suspend fun getFollowersList(
+        @Header("Authorization") authorization: String,
+        @Path("email") email: String
+    )
+
+    @POST("profile/{email}/following")
+    suspend fun doFollow(
+        @Header("Authorization") authorization: String,
+        @Path("email") email: String
+    ): Call<Void>
+
+    @DELETE("profile/{email}/following")
+    suspend fun cancelFollow(
+        @Header("Authorization") authorization: String,
+        @Path("email") email: String
+    ): Call<Void>
 }
