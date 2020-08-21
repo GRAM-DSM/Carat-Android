a516dd87b32bba4246bc476dcfe5f4346f4916bc
minhee <jwmh15@gmail.com>
2020-08-22 01:49:06 +0900

    Add Service

--- app/src/main/java/com/example/carat/Network/CaratService.kt
+++ app/src/main/java/com/example/carat/Network/CaratService.kt
@@ -1,15 +1,64 @@
 package com.example.carat.Network
 
+import com.example.carat.Model.ServerMessage
 import com.example.carat.Model.TokenData
 import okhttp3.ResponseBody
-import retrofit2.http.Field
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
+import javax.sql.StatementEvent
 
 interface CaratService {
     @POST("/user/auth")
-    suspend fun login(@Field("password") password: String, @Field("email") email: String) : TokenData
+    suspend fun login(
+        @Field("password") password: String,
+        @Field("email") email: String
+    ) : TokenData
 
-    @GET("/user/auth")
-    suspend fun reissueToken() : ResponseBody
+    @POST ("/user")
+    suspend fun signUp(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field ("password") password: String
+    ) : ServerMessage
+
+    @DELETE("/user")
+    suspend fun deleteUser(
+        @Header("Authorization") authorization: String
+    ): ServerMessage
+
+    @GET("/timeline")
+    suspend fun getTimeLine(
+        @Field("size") size: Int,
+        @Field("last_caring_id") caring_id: Int
+    )
+
+    @POST("/recaring")
+    suspend fun recaring(
+        @Header("Authorization") Authorization: String,
+        @Field("id") id: Int
+    ) : ServerMessage
+
+    @DELETE("/recaring")
+    suspend fun deleteRecaring(
+        @Header("Authorization") Authorization: String,
+        @Field("id") caring_id: Int
+    ) : ServerMessage
+
+    @POST("/carat/<id>")
+    suspend fun like(
+        @Path("id") id: Int,
+        @Header("Authorization") Authorization: String
+    ) : ServerMessage
+
+    @DELETE("/carat/<id>")
+    suspend fun deleteCarat(
+        @Path("id") id: Int,
+        @Header("Authorization") Authorization: String
+    ) : ServerMessage
+
+    @GET("/carat/<id>/list")
+    suspend fun caratList(
+        @Path("id") id: Int,
+        @Header("Authorization") Authorization: String
+    )
+
 }
