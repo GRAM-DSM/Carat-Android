f9111889c7c51fe423deb9c9c32b8234e6232230
minhee <jwmh15@gmail.com>
2020-08-22 01:53:57 +0900

    Modify CaratService

--- app/src/main/java/com/example/carat/Network/CaratService.kt
+++ app/src/main/java/com/example/carat/Network/CaratService.kt
@@ -8,57 +8,8 @@ import javax.sql.StatementEvent
 
 interface CaratService {
     @POST("/user/auth")
-    suspend fun login(
-        @Field("password") password: String,
-        @Field("email") email: String
-    ) : TokenData
+    suspend fun login(@Field("password") password: String, @Field("email") email: String): TokenData
 
-    @POST ("/user")
-    suspend fun signUp(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field ("password") password: String
-    ) : ServerMessage
-
-    @DELETE("/user")
-    suspend fun deleteUser(
-        @Header("Authorization") authorization: String
-    ): ServerMessage
-
-    @GET("/timeline")
-    suspend fun getTimeLine(
-        @Field("size") size: Int,
-        @Field("last_caring_id") caring_id: Int
-    )
-
-    @POST("/recaring")
-    suspend fun recaring(
-        @Header("Authorization") Authorization: String,
-        @Field("id") id: Int
-    ) : ServerMessage
-
-    @DELETE("/recaring")
-    suspend fun deleteRecaring(
-        @Header("Authorization") Authorization: String,
-        @Field("id") caring_id: Int
-    ) : ServerMessage
-
-    @POST("/carat/<id>")
-    suspend fun like(
-        @Path("id") id: Int,
-        @Header("Authorization") Authorization: String
-    ) : ServerMessage
-
-    @DELETE("/carat/<id>")
-    suspend fun deleteCarat(
-        @Path("id") id: Int,
-        @Header("Authorization") Authorization: String
-    ) : ServerMessage
-
-    @GET("/carat/<id>/list")
-    suspend fun caratList(
-        @Path("id") id: Int,
-        @Header("Authorization") Authorization: String
-    )
-
+    @GET("/user/auth")
+    suspend fun reissueToken(@Header("Authorization") authorization: String): ServerMessage
 }
