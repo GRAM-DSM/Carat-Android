c53230cee132c5a7be3d419a7d7d121eaa428698
sirasatarato <sirasatarato@gmail.com>
2020-08-22 13:51:55 +0900

    Create an action to add images

--- app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
+++ app/src/main/java/com/example/carat/Ui/Activity/WritingActivity.kt
@@ -1,9 +1,16 @@
 package com.example.carat.Ui.Activity
 
+import android.content.ClipData
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.marginTop
 import com.bumptech.glide.Glide
 import com.example.carat.Model.UserObject
 import com.example.carat.Presenter.Writing.WritingContract
@@ -13,6 +20,7 @@ import kotlinx.android.synthetic.main.activity_writing.*
 import kotlinx.android.synthetic.main.widget_appbar.view.*
 
 class WritingActivity : AppCompatActivity() {
+    private val PICTURE_REQUEST_CODE = 100
     private val writingPresenter: WritingContract.Presenter = WritingPresenter()
     private val appbar: Toolbar by lazy {
         writing_appbar_include.widget_toolbar
@@ -25,6 +33,7 @@ class WritingActivity : AppCompatActivity() {
         settingActionBar()
         setClickEvents()
         setProfileImage()
+        selectImages()
     }
 
     private fun settingActionBar() {
@@ -54,5 +63,45 @@ class WritingActivity : AppCompatActivity() {
             .load(UserObject.getInstance().profile)
             .circleCrop()
             .into(writing_profile_imageView)
+    }
+
+    private fun selectImages() {
+        linearLayout_writing.setOnLongClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Audio.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            startActivityForResult(
+                Intent.createChooser(intent, "Select Picture"),
+                PICTURE_REQUEST_CODE
+            )
+
+            true
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
+            linearLayout_showImages.removeAllViews()
+            val clipData: ClipData? = data?.clipData
+
+            if (clipData != null) {
+                for (i in 0 until clipData.itemCount) {
+                    val uri: Uri = clipData.getItemAt(i).uri
+                    val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT, 1f
+                    )
+                    
+                    layoutParams.setMargins(0, 10, 0, 0)
+
+                    val imageView = ImageView(this)
+                    imageView.setImageURI(uri)
+                    imageView.layoutParams = layoutParams
+                    linearLayout_showImages.addView(imageView)
+                }
+            }
+        }
     }
 }
