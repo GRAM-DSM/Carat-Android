4af8de5e1f7a1cf933414054bd37f7c51cd8a416
sirasatarato <sirasatarato@gmail.com>
2020-08-31 19:37:43 +0900

    Modify TimeLineAdapter viewholder function

--- app/src/main/java/com/example/carat/Ui/Adapter/TimeLineAdapter.kt
+++ app/src/main/java/com/example/carat/Ui/Adapter/TimeLineAdapter.kt
@@ -1,21 +1,68 @@
 package com.example.carat.Ui.Adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.carat.Model.TimeLinePost
 import com.example.carat.R
 import kotlinx.android.synthetic.main.item_carat_post.view.*
 
-class TimeLineAdapter(val context: Context, val timeLinePostList: ArrayList<TimeLinePost>) :
+class TimeLineAdapter(private val timeLinePostList: ArrayList<TimeLinePost>) :
     RecyclerView.Adapter<TimeLineAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun setImages(itemImages: ArrayList<String>) {
+            itemView.apply {
+                if (itemImages.size == 1) {
+                    Glide.with(context).load(itemImages[0]).into(itemCarat_grid1_imageView)
+                }
+                if (itemImages.size == 2) {
+                    Glide.with(context).load(itemImages[1]).into(itemCarat_grid2_imageView)
+                }
+                if (itemImages.size == 3) {
+                    Glide.with(context).load(itemImages[2]).into(itemCarat_grid3_imageView)
+                }
+                if (itemImages.size == 4) {
+                    Glide.with(context).load(itemImages[3]).into(itemCarat_grid4_imageView)
+                }
+            }
+        }
+
+        fun setCaring(me_recaring: Boolean) {
+            itemView.apply {
+                if (me_recaring) {
+                    itemCarat_reCaring_imageView.setImageResource(R.drawable.icon_re)
+                    itemCarat_reCaring_textView.setTextColor(
+                        ContextCompat.getColor(context, R.color.mainColor)
+                    )
+                } else {
+                    itemCarat_reCaring_imageView.setImageResource(R.drawable.icon_re_gray)
+                    itemCarat_reCaring_textView.setTextColor(
+                        ContextCompat.getColor(context, R.color.gray)
+                    )
+                }
+            }
+        }
+
+        fun setCarats(me_carat: Boolean) {
+            itemView.apply {
+                if (me_carat) {
+                    itemCarat_like_imageView.setImageResource(R.drawable.icon_maiin_logo)
+                    itemCarat_like_textView.setTextColor(
+                        ContextCompat.getColor(context, R.color.mainColor)
+                    )
+                } else {
+                    itemCarat_like_imageView.setImageResource(R.drawable.icon_carat_gray)
+                    itemCarat_like_textView.setTextColor(
+                        ContextCompat.getColor(context, R.color.gray)
+                    )
+                }
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
         LayoutInflater.from(parent.context).inflate(R.layout.item_carat_post, parent, false)
@@ -36,53 +83,9 @@ class TimeLineAdapter(val context: Context, val timeLinePostList: ArrayList<Time
             itemCarat_content_textView.text = item.body
             itemCarat_reCaring_textView.text = item.recaring_count.toString()
             itemCarat_like_textView.text = item.carat_count.toString()
-            setImages(this, item)
-            setCarats(this, item)
-        }
-    }
-
-    private fun setImages(view: View, item: TimeLinePost) {
-        val image = item.body_images
-
-        view.apply {
-            if (image.size == 1) {
-                Glide.with(context).load(item.body_images[0]).into(itemCarat_grid1_imageView)
-            }
-            if (image.size == 2) {
-                Glide.with(context).load(item.body_images[1]).into(itemCarat_grid2_imageView)
-            }
-            if (image.size == 3) {
-                Glide.with(context).load(item.body_images[2]).into(itemCarat_grid3_imageView)
-            }
-            if (image.size == 4) {
-                Glide.with(context).load(item.body_images[3]).into(itemCarat_grid4_imageView)
-            }
-        }
-    }
-
-    private fun setCarats(view: View, item: TimeLinePost) {
-        if (item.me_recaring) {
-            view.itemCarat_reCaring_imageView.setImageResource(R.drawable.icon_re)
-            view.itemCarat_reCaring_textView.setTextColor(
-                ContextCompat.getColor(context, R.color.mainColor)
-            )
-        } else {
-            view.itemCarat_reCaring_imageView.setImageResource(R.drawable.icon_re_gray)
-            view.itemCarat_reCaring_textView.setTextColor(
-                ContextCompat.getColor(context, R.color.gray)
-            )
-        }
-
-        if (item.me_carat) {
-            view.itemCarat_like_imageView.setImageResource(R.drawable.icon_maiin_logo)
-            view.itemCarat_like_textView.setTextColor(
-                ContextCompat.getColor(context, R.color.mainColor)
-            )
-        } else {
-            view.itemCarat_like_imageView.setImageResource(R.drawable.icon_carat_gray)
-            view.itemCarat_like_textView.setTextColor(
-                ContextCompat.getColor(context, R.color.gray)
-            )
+            holder.setImages(item.body_images)
+            holder.setCarats(item.me_recaring)
+            holder.setCaring(item.me_carat)
         }
     }
 }
