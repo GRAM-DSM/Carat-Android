0d6d007155e41447923227e6aa2876c03601f797
minhee <jwmh15@gmail.com>
2020-09-04 02:02:29 +0900

    Add SignInUp Presenter

--- app/src/main/java/com/example/carat/Presenter/SignInUp/CreateUserPresenter.kt
+++ app/src/main/java/com/example/carat/Presenter/SignInUp/CreateUserPresenter.kt
@@ -1,7 +1,10 @@
 package com.example.carat.Presenter.SignInUp
 
+import android.util.Log
+import android.widget.Toast
 import com.example.carat.Repository.Repository
 import com.example.carat.Util.BaseCoroutineScope
+import com.example.carat.Util.MyApp.Companion.context
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -19,8 +22,20 @@ class CreateUserPresenter() :
         }
 
         CoroutineScope(coroutineContext).launch {
-            repository.signUp(hashMap)
+            val result = repository.signUp(hashMap)
+            when(result.message) {
+                "a bad request" -> {
+                    Toast.makeText(context,"이메일이나 비밀번호를 다시 한번 확인해주세요",Toast.LENGTH_SHORT).show()
+                }
+                "That email is already in use." -> {
+                    Toast.makeText(context,"이미 사용중인 이메일입니다.", Toast.LENGTH_SHORT).show()
+                }
+                " " -> {
+                    Log.d("createUserPresenter","회원가입 성공")
+                }
+            }
         }
+
     }
 
 }
--- app/src/main/java/com/example/carat/Presenter/SignInUp/LoginPresenter.kt
+++ app/src/main/java/com/example/carat/Presenter/SignInUp/LoginPresenter.kt
@@ -1,12 +1,17 @@
 package com.example.carat.Presenter.SignInUp
 
+import android.widget.Toast
 import com.example.carat.Repository.Repository
+import com.example.carat.Repository.SharedPreferencesManager
 import com.example.carat.Util.BaseCoroutineScope
+import com.example.carat.Util.MyApp.Companion.context
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 class LoginPresenter(var view: LoginContract.View) : LoginContract.Presenter,
-    BaseCoroutineScope() {
+    BaseCoroutineScope(){
+
+    private val sharedPreferencesManager: SharedPreferencesManager = TODO()
 
     private val repository: Repository = Repository()
     private val hashMap: HashMap<String, String> = HashMap()
@@ -18,7 +23,19 @@ class LoginPresenter(var view: LoginContract.View) : LoginContract.Presenter,
             hashMap["password"] = password
         }
         CoroutineScope(coroutineContext).launch {
-            repository.login(hashMap)
+            val result = repository.login(hashMap)
+            when(result.message) {
+                "The account does not exist" -> {
+                    Toast.makeText(context,"계정이 존재하지 않습니다.",Toast.LENGTH_SHORT).show()
+                }
+                "Email and password do not match" -> {
+                    Toast.makeText(context,"이메일이나 비밀번호를 다시 입력해주세요",Toast.LENGTH_SHORT).show()
+                }
+                else -> {
+                    sharedPreferencesManager.saveToken = result.access_token
+                }
+            }
+
         }
     }
 }
