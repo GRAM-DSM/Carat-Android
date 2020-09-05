045f51b6692baa93cb720e8a5a93efe48b134938
sirasatarato <sirasatarato@gmail.com>
2020-09-05 10:13:18 +0900

    Fix for Timeline to merge

--- app/src/main/java/com/example/carat/Network/CaratService.kt
+++ app/src/main/java/com/example/carat/Network/CaratService.kt
@@ -1,68 +1,127 @@
 package com.example.carat.Network
 
-import androidx.lifecycle.HasDefaultViewModelProviderFactory
+import com.example.carat.Model.*
+import retrofit2.Call
+import retrofit2.http.*
+import kotlin.collections.HashMap
 import com.example.carat.Model.ServerMessage
 import com.example.carat.Model.TokenData
-import retrofit2.http.*
+import okhttp3.MultipartBody
 
 interface CaratService {
-    @POST("/user/auth")
-    suspend fun login(
-        @Body body: HashMap<String,String>
-    ): TokenData
+    @GET("user/auth")
+    suspend fun reissueToken(@Header("Authorization") authorization: String): TokenData
 
-    @GET("/user/auth")
-    suspend fun reissueToken(@Header("Authorization") authorization: String): ServerMessage
-  
-    @POST ("/user")
+    @POST("/user")
     suspend fun signUp(
-        @Body body :HashMap<String, String>
-    ) : ServerMessage
-
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): ServerMessage
 
     @DELETE("/user")
     suspend fun deleteUser(
         @Header("Authorization") authorization: String
     ): ServerMessage
 
-    @GET("/timeline")
-    suspend fun getTimeLine(
-        @Body body: HashMap<String,Int>
-    )
 
-    @GET("/caring/detail/<id>")
-    suspend fun detailCaring(
-        @Path("id") id :String
-    )
+
+    @GET("/timeline")
+    suspend fun getTimeLine(@Body parameter: RequestCaringData): TimeLineData
+
+    @GET("timeline/caring/{email}")
+    suspend fun getCaringTimeLine(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String,
+        @Body parameter: RequestCaringData
+    ): ProfileTimeLinePostData
+
+    @GET("timeline/carat/{email}")
+    suspend fun getCaratTimeLine(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String,
+        @Body parameter: RequestCaringData
+    ): ProfileTimeLinePostData
+
+    @Multipart
+    @POST("/caring")
+    suspend fun createCaring(
+        @Header("Authorization") authorization: String,
+        @Part image: List<MultipartBody.Part>,
+        @Body caring: String
+    ): ServerMessage
+
+    @GET("/caring/detail/{id}")
+    suspend fun detailCaring(@Path("id") id: String): DetailTimeLineData
 
     @POST("/recaring")
-    suspend fun recaring(
+    suspend fun reCaring(
         @Header("Authorization") Authorization: String,
         @Body id: Int
-    ) : ServerMessage
+    ): ServerMessage
 
-    @DELETE("/recaring")
-    suspend fun deleteRecaring(
-        @Path ("id") id: String,
-        @Header("Authorization") Authorization: String
-    ) : ServerMessage
-
-    @POST("/carat/<id>")
-    suspend fun like(
+    @DELETE("/recaring/{id}")
+    suspend fun cancelReCaring(
         @Path("id") id: String,
         @Header("Authorization") Authorization: String
-    ) : ServerMessage
+    ): ServerMessage
 
-    @DELETE("/carat/<id>")
-    suspend fun deleteCarat(
+    @POST("/carat/{id}")
+    suspend fun doCarat(
         @Path("id") id: String,
         @Header("Authorization") Authorization: String
-    ) : ServerMessage
+    ): ServerMessage
 
-    @GET("/carat/<id>/list")
-    suspend fun caratList(
-        @Path("id") id: Int,
+    @DELETE("/carat/{id}")
+    suspend fun cancelCarat(
+        @Path("id") id: String,
         @Header("Authorization") Authorization: String
-    )
+    ): ServerMessage
 
+    @GET("carat/{id}/list")
+    suspend fun getCaratList(
+        @Path("id") id: String,
+        @Header("Authorization") authorization: String
+    ): CaratData
+
+
+
+    @GET("profile/{email}")
+    suspend fun getProfile(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String
+    ): UserData
+
+    @Multipart
+    @PUT("profile")
+    suspend fun modifyProfile(
+        @Header("Authorization") authorization: String,
+        @PartMap parameter: HashMap<String, MultipartBody.Part>,
+        @Part("name") name: String,
+        @Part("about_me") about_me: String
+    ): Call<Unit>
+
+    @POST("profile/{email}/following")
+    suspend fun doFollow(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String
+    ): Call<Unit>
+
+    @DELETE("profile/{email}/following")
+    suspend fun cancelFollow(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String
+    ): Call<Unit>
+
+    @GET("profile/{email}/following")
+    suspend fun getFollowingList(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String
+    ): FollowingData
+
+    @GET("profile/{email}/followers")
+    suspend fun getFollowersList(
+        @Path("email") email: String,
+        @Header("Authorization") authorization: String
+    ): FollowerData
 }
--- app/src/main/java/com/example/carat/Repository/Repository.kt
+++ app/src/main/java/com/example/carat/Repository/Repository.kt
@@ -1,14 +1,16 @@
 package com.example.carat.Repository
 
-import android.content.Context
 import com.example.carat.Model.*
 import com.example.carat.Network.CaratClient
-import com.example.carat.Util.MyApp.Companion.context
+import com.example.carat.Util.MyApp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import okhttp3.MultipartBody
+import retrofit2.Call
+import retrofit2.http.*
 
-class Repository() {
-    private val sharedPreferences = context?.let { SharedPreferencesManager.getInstance(it) }
+class Repository {
+    private val sharedPreferences = SharedPreferencesManager.getInstance(MyApp.context!!)
     private val api = CaratClient.caratApi
 
     fun saveLoginState(isLogin: Boolean) {
@@ -21,32 +23,119 @@ class Repository() {
     }
 
     fun saveEmail() {
-        sharedPreferences?.saveEmail = UserData.getInstance().email
+        sharedPreferences?.saveEmail = UserObject.getInstance().email
     }
 
     fun getLoginState(): Boolean? = sharedPreferences?.isLogin
     fun getAccess(): String? = sharedPreferences?.saveToken
     fun getRefresh(): String? = sharedPreferences?.saveRefreshToken
+    fun getEmail() = sharedPreferences?.saveEmail
 
-    suspend fun signUp(body: HashMap<String, String>): ServerMessage{
-        return withContext(Dispatchers.IO){
-            api.signUp(body)
-        }
-    }
 
-    suspend fun login(body: HashMap<String, String>): TokenData{
-        return withContext(Dispatchers.IO){
-            api.login(body)
-        }
-    }
 
-    suspend fun deleteUser():ServerMessage {
+    suspend fun getTimeLine(parameter: RequestCaringData): TimeLineData {
         return withContext(Dispatchers.IO) {
-            api.deleteUser(TokenData.getInstance().access_token)
+            api.getTimeLine(parameter)
+        }
+    }
+
+    suspend fun getCaringTimeLine(
+        email: String,
+        parameter: RequestCaringData
+    ): ProfileTimeLinePostData {
+        return withContext(Dispatchers.IO) {
+            api.getCaringTimeLine(email, TokenData.getInstance().access_token, parameter)
+        }
+    }
+
+    suspend fun getCaratTimeLine(
+        email: String,
+        parameter: RequestCaringData
+    ): ProfileTimeLinePostData {
+        return withContext(Dispatchers.IO) {
+            api.getCaratTimeLine(email, TokenData.getInstance().access_token, parameter)
+        }
+    }
+
+    suspend fun createCaring(image: List<MultipartBody.Part>, caring: String): ServerMessage {
+        return withContext(Dispatchers.IO) {
+            api.createCaring(TokenData.getInstance().access_token, image, caring)
+        }
+    }
+
+    suspend fun detailCaring(id: String): DetailTimeLineData {
+        return withContext(Dispatchers.IO) {
+            api.detailCaring(id)
+        }
+    }
+
+    suspend fun reCaring(id: Int): ServerMessage {
+        return withContext(Dispatchers.IO) {
+            api.reCaring(TokenData.getInstance().access_token, id)
+        }
+    }
+
+    suspend fun cancelReCaring(id: String): ServerMessage {
+        return withContext(Dispatchers.IO) {
+            api.cancelReCaring(id, TokenData.getInstance().access_token)
+        }
+    }
+
+    suspend fun doCarat(id: String): ServerMessage {
+        return withContext(Dispatchers.IO) {
+            api.doCarat(id, TokenData.getInstance().access_token)
+        }
+    }
+
+    suspend fun cancelCarat(id: String): ServerMessage {
+        return withContext(Dispatchers.IO) {
+            api.cancelCarat(id, TokenData.getInstance().access_token)
+        }
+    }
+
+    suspend fun getCaratList(id: String): CaratData {
+        return withContext(Dispatchers.IO) {
+            api.getCaratList(id, TokenData.getInstance().access_token)
         }
     }
 
 
 
+    suspend fun getProfile(path: String): UserData {
+        return withContext(Dispatchers.IO) {
+            api.getProfile(path, TokenData.getInstance().access_token)
+        }
+    }
 
+    suspend fun updateProfile(
+        hash: HashMap<String, MultipartBody.Part>, name: String, about: String
+    ): Call<Unit> {
+        return withContext(Dispatchers.IO) {
+            api.modifyProfile(TokenData.getInstance().access_token, hash, name, about)
+        }
+    }
+
+    suspend fun getFollowingList(path: String): FollowingData {
+        return withContext(Dispatchers.IO) {
+            api.getFollowingList(path, TokenData.getInstance().access_token)
+        }
+    }
+
+    suspend fun getFollowersList(path: String): FollowerData {
+        return withContext(Dispatchers.IO) {
+            api.getFollowersList(path, TokenData.getInstance().access_token)
+        }
+    }
+
+    suspend fun doFollow(path: String): Call<Unit> {
+        return withContext(Dispatchers.IO) {
+            api.doFollow(path, TokenData.getInstance().access_token)
+        }
+    }
+
+    suspend fun cancelFollow(path: String): Call<Unit> {
+        return withContext(Dispatchers.IO) {
+            api.cancelFollow(path, TokenData.getInstance().access_token)
+        }
+    }
 }
