8296c45822d91ed54c925dc7dd995fe5e6e834a9
minhee <jwmh15@gmail.com>
2020-09-05 09:50:10 +0900

    Add DetailCarat

--- app/src/main/java/com/example/carat/Presenter/TimeLine/TimeLineContract.kt
+++ app/src/main/java/com/example/carat/Presenter/TimeLine/TimeLineContract.kt
@@ -1,10 +1,12 @@
 package com.example.carat.Presenter.TimeLine
 
 import com.example.carat.Model.TimeLineData
+import com.example.carat.Ui.Adapter.TimeLineAdapter
 
 interface TimeLineContract {
 
     interface View {
+        fun setTimeLineAdapter(timeLineAdapter: TimeLineAdapter)
 
     }
 
